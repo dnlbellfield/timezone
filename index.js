0d512b798f5f45d4feebad5f6b65bf37f5ec4128
Daniel Bellfield <dnlbellfield@gmail.com>
2021-10-29 10:38:05 -0700

    time-zone import

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import moment from 'moment';
 import lodash from 'lodash';
+import momentTZ from 'moment-timezone';
 
-const snapshot = moment().format();
-var date = new moment();
-console.log('date . date :', date.date());
-console.log('date :', date)
-console.log('moment().format() :', moment().format());
 
-var formatted = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); 
-console.log('formatted :', formatted)
-console.log(lodash.create);
+var current = momentTZ.tz.setDefault('America/Los_Angeles');
+console.log('current :', 'America/Los_Angeles')
+
+
+
+
+
+
+
+
+
+// https://www.youtube.com/watch?v=B047pkqRCak&list=PLqq-6Pq4lTTa-d0iZg41U2RDqECol9C5B&index=15
+// const snapshot = moment().format();
+// var date = new moment();
+// console.log('date . date :', date.date());
+// console.log('date :', date)
+// console.log('moment().format() :', moment().format());
+
+// var formatted = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); 
+// console.log('formatted :', formatted)
+// console.log(lodash.create);
 // console.log("Hello World");
